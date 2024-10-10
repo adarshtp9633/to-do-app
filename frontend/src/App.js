079c2df4c3ab3f9f7abf701ec5923eb0aaccaf0f
adarshtp9633 <141206398+adarshtp9633@users.noreply.github.com>
2024-10-10 15:21:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="main">
       <header className="title">
-        <h1>Todo-Application </h1>
+        <h1>Todo-adarsh </h1>
       </header>
 
       <div className='taskInputContanier'>
